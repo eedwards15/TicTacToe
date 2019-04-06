ad023bda84cd19270880301cb50e95f756080935
Alex <alexwerewolf2001@yahoo.com>
2019-04-06 12:18:19 -0400

    Created Game Loop And Some basic logic for TicTacToe Game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut board:[[u8; 3]; 3] = [[0; 3] ; 3];
 
@@ -7,16 +9,57 @@ fn main() {
         }
     }   
 
-    display_board(&mut board);
-    println!("-------------------");
-    set_values(5,1,1,&mut board); 
-    display_board(&mut board);
-    
+    loop 
+    {
+        if(check_board(board)){
+            break; 
+        }
+
+        println!("Please Enter row");
+        let mut row = String::new(); 
+        io::stdin().read_line(&mut row)
+            .expect("Failed to read line"); 
+
+        let row: usize = match row.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("Please Enter column"); 
+        let mut col = String::new(); 
+        io::stdin().read_line(&mut col)
+            .expect("Failed to read line"); 
+
+            
+        let col: usize = match col.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        println!("Please Enter Player"); 
+        let mut player = String::new(); 
+        io::stdin().read_line(&mut player)
+            .expect("Failed to read line"); 
+
+        let player: u8 = match player.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        set_values(player,col,row,&mut board);
+        display_board(&mut board);
+
+    }
 
 }
 
-fn set_values(value: u8 ,col:usize, row:usize, board: &mut [[u8; 3];3]){
-    board[row][col] = value;
+fn set_values(value: u8 ,col:usize, row:usize, board: &mut [[u8; 3];3]) -> bool{
+    if board[col][row] == 0 
+    {
+        board[col][row] = value;
+        return true; 
+    }
+    return false; 
 }
 
 
@@ -29,3 +72,15 @@ fn display_board(board: &mut [[u8; 3];3]){
     }
 }
 
+fn check_board(board: [[u8;3];3] ) -> bool{
+    for x in 0..3 {
+        for y in 0..3
+        {
+            if board[y][x] == 0
+            {
+            return  false; 
+            }
+        }   
+    }
+
+    return true; 
